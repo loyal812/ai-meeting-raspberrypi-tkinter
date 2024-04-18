ff8eea384829c726e459c3528a27ed39e6f6a9e8
loyal812 <jh.chan0812@gmail.com>
2024-04-18 08:23:14 -0400

    feat: start ui using tkinter

---
+++ main.py
@@ -0,0 +1,15 @@
+# Import Module
+from tkinter import *
+
+# create root window
+root = Tk()
+
+# root window title and dimension
+root.title("Automatic meeting summary")
+root.config(bg="skyblue")
+# Set geometry (widthxheight)
+root.geometry('586x330')
+
+# all widgets will be here
+# Execute Tkinter
+root.mainloop()
