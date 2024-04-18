ed1ea828a3741300ac9ae7f218587267e8ebcec0
loyal812 <jh.chan0812@gmail.com>
2024-04-18 08:26:11 -0400

    feat: implement the logo image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Import Module
 from tkinter import *
+from PIL import Image, ImageTk
 
 class GradientFrame(Canvas):
     '''A gradient frame which uses a canvas to draw the background'''
@@ -33,24 +34,39 @@ class GradientFrame(Canvas):
 if __name__ == "__main__":
     # create root window
     root = Tk()
-
-    # root window title and dimension
-    root.title("Automatic meeting summary")
-    # root.config(bg="skyblue")
-    # Set geometry (widthxheight)
     root.geometry('586x330')
 
-    # Create Frame widget
-    head_frame = Frame(root, width=586, height=50, bg='#2C2C2C')
-    head_frame.grid(row=0, column=0, sticky="ew")
+    # Optional: Remove window decorations for a true full-screen experience
+    # root.overrideredirect(True)
+    root.resizable(width=False, height=False)
 
-    
-    main_frame = Frame(root, width=586, height=280, bg='blue')
-    main_frame.grid(row=1, column=0, sticky="nsew")  # Add stickiness
-    # main_frame.pack_propagate(False)  # Avoid automatic resizing of the main_frame
-    # grad_main_frame = GradientFrame(main_frame, "#090863", "#1C1AD0", relief="flat")
-    # grad_main_frame.place(relwidth=1, relheight=1)
+    # Main frame with grid-row class
+    main_frame = Frame(root)
+    main_frame.grid(row=0, column=0)
 
-    # all widgets will be here
-    # Execute Tkinter
+    # Sub-frame with h-15 class
+    sub_frame_1 = Frame(main_frame, width=586, height=50, bg='#2C2C2C')  # Assuming height 50 to mimic h-15
+    sub_frame_1.grid(row=0, column=0)
+
+    # Sub-frame within the h-15 frame with grid-col class
+    sub_frame_2 = Frame(sub_frame_1)
+    sub_frame_2.grid(row=0, column=0)
+
+    # Two sub-frames within the grid-col class
+    col_frame_1 = Frame(sub_frame_2, width=150, height=50, bg='#2C2C2C')  # Assuming width 50 to mimic col-span-1
+    col_frame_1.grid(row=0, column=0, padx=(0, 1))
+
+    # load image to be "edited"
+    image = ImageTk.PhotoImage(Image.open("logo.png").resize((100, 30)))
+    Label(col_frame_1, width=145, height=46, image=image, bg='#2C2C2C', highlightbackground='#2C2C2C', highlightcolor='#2C2C2C').pack(fill="both")
+
+    col_frame_2 = Frame(sub_frame_2, width=436, height=50, bg='#2C2C2C')  # Assuming width 150 to mimic col-span-3
+    col_frame_2.grid(row=0, column=1)
+
+    # Sub-frame with h-60 class
+    sub_frame_3 = Frame(main_frame, width=586, height=280, bg='blue')  # Assuming height 180 to mimic h-60
+    sub_frame_3.grid(row=1, column=0)
+
     root.mainloop()
+
+
